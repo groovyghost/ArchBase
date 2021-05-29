ba74db3c38209c79381e47d3398315712acf96d4
Rahul <68483218+ghostr72@users.noreply.github.com>
2021-05-29 16:29:21 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,166 @@
+#!/bin/bash
+
+#encryption_passphrase="arch"
+root_password="arch"
+user_password="arch"
+hostname="archlinux"
+username="arch"
+continent_city="Asia/Kolkata"
+swap_size="1" # same as ram if using hibernation, otherwise minimum of 8
+
+# Set different microcode, kernel params and initramfs modules according to CPU vendor
+cpu_vendor=$(cat /proc/cpuinfo | grep vendor | uniq)
+cpu_microcode=""
+kernel_options=""
+initramfs_modules=""
+if [[ $cpu_vendor =~ "AuthenticAMD" ]]
+then
+ cpu_microcode="amd-ucode"
+ initramfs_modules="amdgpu"
+elif [[ $cpu_vendor =~ "GenuineIntel" ]]
+then
+ cpu_microcode="intel-ucode"
+ kernel_options=" i915.fastboot=1 i915.enable_fbc=1 i915.enable_guc=2"
+ initramfs_modules="intel_agp i915"
+fi
+
+echo "Updating system clock"
+timedatectl set-ntp true
+
+echo "Syncing packages database"
+pacman -Sy --noconfirm
+
+echo "Wiping drive"
+sgdisk --zap-all /dev/sda
+
+echo "Creating partition tables"
+printf "n\n1\n4096\n+512M\nef00\nw\ny\n" | gdisk /dev/sda
+printf "n\n2\n\n\n8e00\nw\ny\n" | gdisk /dev/sda
+
+#echo "Setting up cryptographic volume"
+#mkdir -p -m0700 /run/cryptsetup
+#echo "$encryption_passphrase" | cryptsetup -q --align-payload=8192 -h sha512 -s 512 --use-random --type luks2 -c aes-xts-plain64 luksFormat /dev/sda2
+#echo "$encryption_passphrase" | cryptsetup luksOpen /dev/sda2 cryptlvm
+
+echo "Creating physical volume"
+pvcreate /dev/sda2
+
+echo "Creating volume volume"
+vgcreate vg0 /dev/sda2
+
+echo "Creating logical volumes"
+lvcreate -L +"$swap_size"GB vg0 -n swap
+lvcreate -l +100%FREE vg0 -n root
+
+echo "Setting up / partition"
+yes | mkfs.ext4 /dev/vg0/root
+mount /dev/vg0/root /mnt
+
+echo "Setting up /boot partition"
+yes | mkfs.fat -F32 /dev/sda1
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+
+echo "Setting up swap"
+yes | mkswap /dev/vg0/swap
+swapon /dev/vg0/swap
+
+echo "Installing Arch Linux"
+yes '' | pacstrap /mnt base base-devel efibootmgr linux linux-headers linux-lts linux-lts-headers linux-firmware lvm2 device-mapper dosfstools e2fsprogs $cpu_microcode cryptsetup networkmanager wget man-db man-pages nano vim diffutils lm_sensors
+
+echo "Generating fstab"
+genfstab -U /mnt >> /mnt/etc/fstab
+
+echo "Configuring new system"
+arch-chroot /mnt /bin/bash << EOF
+echo "Setting system clock"
+timedatectl set-ntp true
+timedatectl set-timezone $continent_city
+hwclock --systohc --localtime
+
+echo "Setting locales"
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+locale-gen
+
+echo "Adding persistent keymap"
+echo "KEYMAP=us" > /etc/vconsole.conf
+
+echo "Setting hostname"
+echo $hostname > /etc/hostname
+
+echo "Setting root password"
+echo -en "$root_password\n$root_password" | passwd
+
+echo "Creating new user"
+useradd -m -G wheel,video -s /bin/bash $username
+echo -en "$user_password\n$user_password" | passwd $username
+
+echo "Generating initramfs"
+sed -i 's/^HOOKS.*/HOOKS=(base systemd autodetect keyboard sd-vconsole modconf block sd-encrypt sd-lvm2 filesystems fsck)/' /etc/mkinitcpio.conf
+sed -i 's/^MODULES.*/MODULES=(ext4 $initramfs_modules)/' /etc/mkinitcpio.conf
+mkinitcpio -P
+
+echo "Setting up systemd-boot"
+bootctl --path=/boot install
+
+mkdir -p /boot/loader/
+tee -a /boot/loader/loader.conf << END
+default arch.conf
+timeout 2
+console-mode max
+editor no
+END
+
+mkdir -p /boot/loader/entries/
+touch /boot/loader/entries/arch.conf
+tee -a /boot/loader/entries/arch.conf << END
+title Arch Linux
+linux /vmlinuz-linux
+initrd /$cpu_microcode.img
+initrd /initramfs-linux.img
+options rd.luks.name=$(blkid -s UUID -o value /dev/sda2)=cryptlvm root=/dev/vg0/root resume=/dev/vg0/swap rd.luks.options=discard$kernel_options nmi_watchdog=0 quiet rw
+END
+
+touch /boot/loader/entries/arch-lts.conf
+tee -a /boot/loader/entries/arch-lts.conf << END
+title Arch Linux LTS
+linux /vmlinuz-linux-lts
+initrd /$cpu_microcode.img
+initrd /initramfs-linux-lts.img
+options rd.luks.name=$(blkid -s UUID -o value /dev/sda2)=cryptlvm root=/dev/vg0/root resume=/dev/vg0/swap rd.luks.options=discard$kernel_options nmi_watchdog=0 quiet rw
+END
+
+echo "Setting up Pacman hook for automatic systemd-boot updates"
+mkdir -p /etc/pacman.d/hooks/
+touch /etc/pacman.d/hooks/systemd-boot.hook
+tee -a /etc/pacman.d/hooks/systemd-boot.hook << END
+[Trigger]
+Type = Package
+Operation = Upgrade
+Target = systemd
+
+[Action]
+Description = Updating systemd-boot
+When = PostTransaction
+Exec = /usr/bin/bootctl update
+END
+
+echo "Setting swappiness to 20"
+touch /etc/sysctl.d/99-swappiness.conf
+echo 'vm.swappiness=20' > /etc/sysctl.d/99-swappiness.conf
+
+echo "Enabling periodic TRIM"
+systemctl enable fstrim.timer
+
+echo "Enabling NetworkManager"
+systemctl enable NetworkManager
+
+echo "Adding user as a sudoer"
+echo '%wheel ALL=(ALL) ALL' | EDITOR='tee -a' visudo
+EOF
+
+umount -R /mnt
+swapoff -a
+
+echo "Arch Linux is ready. You can reboot now!"
