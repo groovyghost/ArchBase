80d1d36c645395ecdc6ea006eb41f265c3a37372
Rahul <68483218+ghostr72@users.noreply.github.com>
2021-08-17 13:29:49 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -94,7 +94,7 @@ echo "Setting root password"
 echo -en "$root_password\n$root_password" | passwd
 
 echo "Installing packages"
-pacman -Sy --noconfirm grub efibootmgr dhcpcd networkmanger vim linux-headers
+pacman -Sy --noconfirm grub efibootmgr dhcpcd networkmanger vim linux-headers wget git
 
 echo "Configuring grub"
 grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=ArchLinux
